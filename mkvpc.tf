7b568f941181163cd2564bde779839e5cc5db647
OlegShmyrin <oleg.shmyrin@gmail.com>
2021-09-23 23:07:49 +0300

    add tf

--- mkvpc.tf
+++ mkvpc.tf
@@ -1,7 +1,5 @@
 provider "aws" {
-   # access_key = "${var.AWS_ACCESS_KEY_ID}"
-   # secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
-   # region = "${var.AWS_DEFAULT_REGION}"
+   region = "eu-central-1"
 }
 
 resource "aws_vpc" "TEST" {
