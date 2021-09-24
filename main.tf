561e717264fece59a2d4e25439bda8c4e7d73f9d
OlegShmyrin <oleg.shmyrin@gmail.com>
2021-09-24 10:47:55 +0300

    add main.tf with appr=server

---
+++ main.tf
@@ -0,0 +1,16 @@
+
+provider "aws" {
+   region = "eu-central-1"
+  # secret_key = "${AWS_SECRET_ACCESS_KEY}"
+  # access_key = "${AWS_ACCESS_KEY_ID}"
+}
+
+resource "aws_instance" "AppServer" {
+  ami           = "ami-05f7491af5eef733a" # Ubuntu 20
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "PetClinicAppSRV"
+    Owner = "Oleg Shmyrin"
+  }
+}
