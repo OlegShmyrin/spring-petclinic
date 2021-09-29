30315df8bc4ecf5a9c7d4aac3fe8f75ca20d44fb
OlegShmyrin <oleg.shmyrin@gmail.com>
2021-09-29 22:59:27 +0300

    Generate Key Pair

--- outputs.tf
+++ outputs.tf
@@ -4,8 +4,8 @@ resource "local_file" "AnsibleInventory" {
      AppServer-dns=aws_instance.AppServer.private_dns,
      #AppServer-ip=aws_instance.AppServer.public_ip,
      AppServer-pr-ip=aws_instance.AppServer.private_ip,
-     AppServer-id=aws_instance.AppServer.id,
-     AppServer-key=aws_key_pair.generated_key.generated_key_name}
+     AppServer-id=aws_instance.AppServer.id}
+     #AppServer-key=aws_key_pair.generated_key.generated_key_name}
  )
  filename = "inventory"
 }
