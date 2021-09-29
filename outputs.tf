25b3ba4ead1adf370c6bb3e4c4a450054b332f54
OlegShmyrin <oleg.shmyrin@gmail.com>
2021-09-29 16:45:50 +0300

    make inventory file from TF outputs

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,6 @@
 ### The Ansible inventory file
 resource "local_file" "AnsibleInventory" {
- content = templatefile("inventory.tmpl",
- {
-  AppServer-dns = aws_instance.AppServer.private_dns,
-  AppServer-ip = aws_instance.AppServer.public_ip,
-  #private-ip = aws_instance.AppServer.*.private_ip,
-  AppServer-id = aws_instance.AppServer.id
- }
+ content = templatefile("inventory.tmpl",{AppServer-dns=aws_instance.AppServer.private_dns,AppServer-ip=aws_instance.AppServer.public_ip,AppServer-id=aws_instance.AppServer.id}
  )
  filename = "inventory"
 }
